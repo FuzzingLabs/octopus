cd5046c7461cb04ee68a1776a149e23c019df749
Patrick Ventuzelo <patrick.ventuzelo@quoscient.io>
2018-06-29 09:30:21 +0200

    add new api contract class

--- octopus/api/contract.py
+++ octopus/api/contract.py
@@ -1,8 +1,24 @@
 class Contract(object):
 
-    def __init__(self, name="default_contract_name", address=None, balance=None, bytecode=None, abi=None, cfg=None):
+    def __init__(self, name="default_contract_name",
+                 address=None, balance=None, bytecode=None,
+                 abi=None, source_code=None):
         self.name = name
         self.address = address
         self.balance = balance
         self.bytecode = bytecode
         self.abi = abi
+        self.source_code = source_code
+        self.info = {}
+
+    def get_online_bytecode(self, explorer):
+        raise NotImplementedError
+
+    def get_online_abi(self, explorer):
+        raise NotImplementedError
+
+    def get_online_source(self, explorer):
+        raise NotImplementedError
+
+    def get_online_info(self, explorer):
+        raise NotImplementedError
