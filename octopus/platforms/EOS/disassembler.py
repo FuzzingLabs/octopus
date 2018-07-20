4d9251b1e65e7255492574abe11316f54894712c
Patrick Ventuzelo <patrick.ventuzelo@quoscient.io>
2018-07-20 17:20:59 +0200

    fix bug eos disasm

--- octopus/platforms/EOS/disassembler.py
+++ octopus/platforms/EOS/disassembler.py
@@ -3,7 +3,6 @@ from octopus.arch.wasm.disassembler import WasmDisassembler
 
 # Eos smart contract == wasm module
 class EosDisassembler(WasmDisassembler):
-    def __init__(self, bytecode=None, asm=None):
+    def __init__(self, bytecode=None):
         WasmDisassembler.__init__(self,
-                                  bytecode=bytecode,
-                                  asm=asm)
+                                  bytecode=bytecode)
